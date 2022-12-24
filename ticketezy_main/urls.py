28c18a002f4fde84630a7af7bff1e0bbac077980
Sanjay M <arjunsanjay0@gmail.com>
2022-12-24 11:41:04 +0530

    created new drf project

---
+++ ticketezy_main/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
+]
